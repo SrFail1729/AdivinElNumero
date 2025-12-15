32171e30ba39ec6c67daf8996b2efb135b498ce5
ivabermel <ivanbermejomelia@gmail.com>
2025-12-15 17:33:14 +0100

    implementada la función comunicación con el servidor

--- src/main/java/Cliente/ClienteTCP.java
+++ src/main/java/Cliente/ClienteTCP.java
@@ -13,38 +13,53 @@ public class ClienteTCP {
 
 
     private static void menu(){
+        boolean continuar = true;
+        while (continuar){
+            try {
+                BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
 
-        try {
-            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-
-            System.out.print("""
+                System.out.print("""
                 1. Nueva partida.
                 
                 2. Salir.
                 """);
 
-            String usuario = teclado.readLine();
+                String usuario = teclado.readLine();
 
-            int opcion = Integer.parseInt(usuario);
+                int opcion = Integer.parseInt(usuario);
 
-            switch (opcion){
-                case 1:
+                switch (opcion){
+                    case 1:
+                        comunicaciónServidor();
+                        break;
+                    case 2:
+                        continuar = false;
+                        break;
 
-                    break;
+                    default:
+                        System.err.println("El valor introducido no esta contemplado.");
 
-                case 2:
+                }
 
-                    break;
-
-                default:
-                    System.err.println("El valor introducido no esta contemplado.");
-
+            }catch (IOException e){
+                System.err.println("Error de IO: " + e.getMessage());
             }
-
-        }catch (IOException e){
-            System.err.println("Error de IO: " + e.getMessage());
         }
     }
 
+    private static  void comunicaciónServidor(){
+        try(Socket socket = new Socket("localhost",65000)){
 
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+            out.writeUTF(teclado.readLine());
+
+            out.flush();
+            socket.shutdownInput();
+
+        }catch (IOException e){
+            System.err.println("Error de comunicación" + e.getMessage());
+        }
+    }
 }
